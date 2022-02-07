b4844166ce5588d23dd45cc767b3f64cf6dabace
ikir0806 <ikir2826@gmail.com>
2022-02-07 22:57:59 +0300

    complete lesson-5

--- script2.js
+++ script2.js
@@ -1,24 +1,66 @@
-let product1 = {
-    title: 'shirt',
-    price: 700
-};
-
-let product2 = {
-    title: 'jeans',
-    price: 2000
-}
-
-let cart = {
-    product1: product1,
-    product2: product2,
-}
-
-countBasketPrice(cart);
-
-function countBasketPrice(object) {
-    let price = 0;
-    for (const key in object) {
-        price += object[key].price;
+const catalog = {
+    1: {
+        id: 1,
+        title: 'shirt',
+        price: 700,
+    },
+    2: {
+        id: 2,
+        title: 'jeans',
+        price: 2000,
+    },
+    3: {
+        id: 3,
+        title: 'sneakers',
+        price: 5000,
     }
-    console.log(price);
 }
+
+const cart = {
+    items: [
+        {
+            id: 1,
+            quantity: 3
+        },
+        {
+            id: 2,
+            quantity: 1
+        },
+        {
+            id: 3,
+            quantity: 2
+        }
+    ],
+
+    countTotalPrice() {
+        let result = 0;
+
+        for (let item of this.items) {
+            result += catalog[item.id].price;
+        }
+
+        return result;
+    },
+
+    countTotalQuantity() {
+        let result = 0;
+
+        for (let item of this.items) {
+            result += item.quantity;
+        }
+
+        return result;
+    }
+}
+
+let m = cart.countTotalPrice();
+let n = cart.countTotalQuantity();
+
+let element = document.getElementById('cart');
+
+if (n == 0) {
+    element.innerHTML = 'Ваша корзина пуста';
+} else {
+    element.innerHTML = `В корзине: ${n} товаров на сумму ${m} рублей`;
+}
+document.body.appendChild(element);
